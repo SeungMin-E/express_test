0f2a08162b848d236a4199ad20b6a16bb5c49ead
rainy2368 <rainy2368@gmail.com>
2023-09-01 17:47:49 +0900

    rere

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ var mysql = require("mysql2");
 var db_info = {
     host: "fail.cljd5jhrse53.ap-northeast-2.rds.amazonaws.com", // 데이터베이스 주소
     port: "33064", // 데이터베이스 포트
-    user: "effail", // 로그인 계정
+    user: "elfail", // 로그인 계정
     password: "leejoon-4", // 비밀번호
     database: "nsa", // 엑세스할 데이터베이스
   };
